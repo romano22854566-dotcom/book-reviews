f3cd6558e31bfe95d641b252ab73e6750669bc88
PeTkun_Industries <romano22854566@gmail.com>
2026-04-02 22:29:07 +0300

    COMMIT

--- src/test/java/com/example/bookreviews/BookReviewsApplicationTests.java
+++ src/test/java/com/example/bookreviews/BookReviewsApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 @Disabled("Отключено, так как в CI/CD нет поднятой БД PostgreSQL")
+
 @SpringBootTest
 class BookReviewsApplicationTests {
     @Disabled
